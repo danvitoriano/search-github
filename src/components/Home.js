cf9b5f542d634c16f1e0d2cf4819d23ab5da82d2
Danilo Vitoriano <vitoriano08@gmail.com>
2018-08-10 11:48:17 -0300

    add home component route

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Text from "./Text";
+
+// home render
+const Home = () => (
+  <div className="App">
+    <Text type="title" fontSize="80" />
+  </div>
+);
+
+export default Home;
