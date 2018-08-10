0d215c30b940c7ed6ecdf3cd6d54a10e615784f4
Danilo Vitoriano <vitoriano08@gmail.com>
2018-08-10 11:06:53 -0300

    add global styles and fonts

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/App";
+import "./components/styles/global.css";
 
 // unique render by react dom
 render(
